29f16b7e0b3d41956ee044a393eb84667c275b98
Erik Dittrich <erik.dittrich@gmail.com>
2021-12-30 14:20:53 +0100

    Added database backend

--- spike_django/microblog/models.py
+++ spike_django/microblog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class BlogProject(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+    project = models.ForeignKey(BlogProject,
+                                on_delete=models.CASCADE,
+                                related_name='posts')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True,
+                               related_name='posts')
+    date = models.DateTimeField(default=timezone.now)
+    text = models.TextField()
+
+    def __str__(self):
+        return "{}: {}...".format(self.project.name, self.text[:100])
